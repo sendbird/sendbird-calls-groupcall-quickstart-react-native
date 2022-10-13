8c7bb9af8f844504b93034f54b9ceca001d00fbd
bang9 <gusrn1423@naver.com>
2022-10-13 11:28:50 +0900

    fix build

--- android/app/src/main/java/com/groupcall/MainActivity.java
+++ android/app/src/main/java/com/groupcall/MainActivity.java
@@ -1,8 +1,11 @@
 package com.groupcall;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+
 
 public class MainActivity extends ReactActivity {
 
